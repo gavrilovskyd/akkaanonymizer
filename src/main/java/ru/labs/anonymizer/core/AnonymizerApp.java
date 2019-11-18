dbe3f134b3bb7abcdccaec8c1fa6adf94f00538d
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:18:50 +0300

    Scripted auto-commit on change (2019-11-18 21:18:50) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
@@ -19,7 +19,7 @@ public class AnonymizerApp {
             System.exit(-1);
         }
         String host = args[0];
-        String port = Integer.parseInt(args[1]);
+        int port = Integer.parseInt(args[1]);
 
         ActorSystem system = ActorSystem.create("anonymizer-system");
 

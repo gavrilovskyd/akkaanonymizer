1322c5d6a02c87b509fedd13fa2781593b90174e
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 22:37:21 +0300

    Scripted auto-commit on change (2019-11-18 22:37:21) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
+++ src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
@@ -62,7 +62,7 @@ public class ServiceDiscovery {
             });
 
             List<String> addresses = new ArrayList<>();
-            servers.forEach((server) -> {
+            servers.forEach(server -> {
                 byte[] addr = zoo.getData(REGISTRY_ROOT + "/" + server, false, null);
             });
         } catch (KeeperException | InterruptedException e) {

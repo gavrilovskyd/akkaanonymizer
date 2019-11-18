f5c90633d00fdbd40ec4a5196eb8a1cf822b63d3
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:02:27 +0300

    Scripted auto-commit on change (2019-11-18 21:02:27) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
@@ -50,7 +50,7 @@ public class AnonymizerRoutes extends AllDirectives {
     private CompletionStage<HttpResponse> redirect(String url, int count) {
         return Patterns.ask(hostStoreActor, new GetRandomHostMessage(), TIMEOUT)
             .thenCompose(serverNameParam -> {
-
+                String serverName = ((String) serverNameParam);
             });
     }
 }

71e102d7190754e1783ecaf5cf5e66b461da9ed2
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 20:24:29 +0300

    Scripted auto-commit on change (2019-11-18 20:24:29) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/messages/RemoveServerMessage.java
+++ src/main/java/ru/labs/anonymizer/messages/RemoveServerMessage.java
@@ -1,4 +1,13 @@
 package ru.labs.anonymizer.messages;
 
 public class RemoveServerMessage {
+    private String serverName;
+
+    public RemoveServerMessage(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
 }

3de52674b8fb1efb27a44cfd7babfc1496d6b548
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:36:11 +0300

    Scripted auto-commit on change (2019-11-18 21:36:11) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerServer.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerServer.java
@@ -3,6 +3,7 @@ package ru.labs.anonymizer.core;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.model.Uri;
 import akka.http.javadsl.server.Route;
 import org.apache.zookeeper.KeeperException;
 import ru.labs.anonymizer.actors.AddressStoreActor;
@@ -20,7 +21,7 @@ public class AnonymizerServer {
 
         ActorRef hostStoreActor = system.actorOf(Props.create(AddressStoreActor.class), "host-store");
         this.serviceDiscovery = new ServiceDiscovery(zkHost, hostStoreActor);
-        this.serviceDiscovery.register(SCHEME + "://" +host+":"+port);
+        this.serviceDiscovery.register(Uri.create(host).scheme(SCHEME).port(port).toString());
 
         this.routes = new AnonymizerRoutes(system, hostStoreActor);
     }

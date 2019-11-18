d65895776e4d6133ed30e8e9c02b0dd8aacb7ce1
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 14:39:21 +0300

    Scripted auto-commit on change (2019-11-18 14:39:21) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
+++ src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
@@ -23,7 +23,11 @@ public class ServiceDiscovery {
 
        List<String> servers = zoo.getChildren(REGISTRY_ROOT, false);
        servers.forEach((addr) ->
-           serversStorageActor.tell(new ChangeServerListMessage(addr, ChangeServerListMessage.EventType.ADD), ActorRef.noSender()));
+           serversStorageActor.tell(
+               new ChangeServerListMessage(addr, ChangeServerListMessage.EventType.ADD),
+               ActorRef.noSender()
+           )
+       );
     }
 
     public void register(String host) throws KeeperException, InterruptedException {

52f5495c6e137569e2c67a8b4d90ccd4cbe36abd
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 13:47:11 +0300

    Scripted auto-commit on change (2019-11-18 13:47:11) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
+++ src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
@@ -2,12 +2,14 @@ package ru.labs.anonymizer.core;
 
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+
 public class ServiceDiscovery {
     private static final int SESSION_TIMEOUT = 3000; // ms
 
     private ZooKeeper zoo;
 
-    public ServiceDiscovery(String zkHost) {
+    public ServiceDiscovery(String zkHost) throws IOException {
         this.zoo = new ZooKeeper(zkHost, SESSION_TIMEOUT, this);
     }
 }

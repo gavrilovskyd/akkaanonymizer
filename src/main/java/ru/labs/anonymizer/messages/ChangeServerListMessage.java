ff48e022a4cb7bf6576f303eac2950d71eb6b2f2
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 14:36:19 +0300

    Scripted auto-commit on change (2019-11-18 14:36:19) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/messages/ChangeServerListMessage.java
+++ src/main/java/ru/labs/anonymizer/messages/ChangeServerListMessage.java
@@ -1,6 +1,11 @@
 package ru.labs.anonymizer.messages;
 
 public class ChangeServerListMessage {
+    public enum EventType {
+        ADD,
+        REMOVE
+    };
+
     private String serverAddr;
-    private 
+    private
 }

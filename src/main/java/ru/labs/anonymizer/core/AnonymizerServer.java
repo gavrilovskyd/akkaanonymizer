ab00b039e88cd244d015a98ed971f638f0e1782a
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:11:45 +0300

    Scripted auto-commit on change (2019-11-18 21:11:45) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerServer.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerServer.java
@@ -2,11 +2,13 @@ package ru.labs.anonymizer.core;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import ru.labs.anonymizer.actors.HostStoreActor;
 
 public class AnonymizerServer {
     private AnonymizerRoutes routes;
 
     public AnonymizerServer(ActorSystem system) {
-        ActorRef hostStoreActor = system
+        ActorRef hostStoreActor = system.actorOf(Props.create(HostStoreActor.class), "host-store");
     }
 }

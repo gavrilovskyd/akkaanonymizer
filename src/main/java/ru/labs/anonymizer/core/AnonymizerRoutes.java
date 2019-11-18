935efd7850197dfb61e9dc5033aa87e7bbab9c40
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 12:10:50 +0300

    Scripted auto-commit on change (2019-11-18 12:10:50) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
@@ -1,12 +1,12 @@
 package ru.labs.anonymizer.core;
 
-import akka.actor.ActorSystem;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 
 public class AnonymizerRoutes extends AllDirectives {
     public AnonymizerRoutes() {}
 
     public Route route() {
-        
+
     }
 }

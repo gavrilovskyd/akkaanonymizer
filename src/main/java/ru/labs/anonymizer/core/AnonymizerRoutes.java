a22171642360432c5c5ac2ee0942a7dc14ab8154
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 20:45:39 +0300

    Scripted auto-commit on change (2019-11-18 20:45:39) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
@@ -13,9 +13,7 @@ public class AnonymizerRoutes extends AllDirectives {
                     get(() ->
                         parameter("url", (url) ->
                             parameter("count", (count)-> {
-                                int countParam = Integer.parseInt(
-                                    httpRequest.getUri().query().getOrElse(COUNT_PARAMETER_NAME, COUNT_DEFAULT_VALUE)
-                                );
+                                int countParam = Integer.parseInt(count);
                             })
                         ))
                 ))

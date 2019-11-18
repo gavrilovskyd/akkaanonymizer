efc8448e318ceffe6baeb9137dbae0585a6deba1
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:15:11 +0300

    Scripted auto-commit on change (2019-11-18 21:15:10) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
@@ -1,8 +1,16 @@
 package ru.labs.anonymizer.core;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class AnonymizerApp {
     public static void main(String[] args) throws Exception {
@@ -10,5 +18,12 @@ public class AnonymizerApp {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        AnonymizerServer server = new AnonymizerServer(system);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.routes().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+            routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
+
+        System.out.println("Server started at http://localhost:8080/");
     }
 }

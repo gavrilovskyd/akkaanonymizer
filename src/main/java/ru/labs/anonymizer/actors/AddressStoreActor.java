034867973bf4032b2462c7485bb88416d40baa4b
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 21:52:57 +0300

    Scripted auto-commit on change (2019-11-18 21:52:57) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/actors/AddressStoreActor.java
+++ src/main/java/ru/labs/anonymizer/actors/AddressStoreActor.java
@@ -1,6 +1,8 @@
 package ru.labs.anonymizer.actors;
 
 import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 import ru.labs.anonymizer.messages.AddAddressMessage;
 import ru.labs.anonymizer.messages.GetRandomAddressMessage;
@@ -11,6 +13,7 @@ import java.util.Random;
 
 public class AddressStoreActor extends AbstractActor {
     private HashMap<String, String> hostsStorage = new HashMap<>();
+    private LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
 
     @Override
     public Receive createReceive() {

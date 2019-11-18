f2c078b9ac03fb408296db6378e6bd1c1c3d308f
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 13:46:01 +0300

    Scripted auto-commit on change (2019-11-18 13:46:01) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
+++ src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
@@ -3,6 +3,8 @@ package ru.labs.anonymizer.core;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ServiceDiscovery {
+    private static final int TIME
+
     private ZooKeeper zoo;
 
     public ServiceDiscovery(String zkHost) {

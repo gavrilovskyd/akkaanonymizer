4ba69838a90c01bb7f01a760bc047b2ab66b19aa
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 12:06:59 +0300

    Scripted auto-commit on change (2019-11-18 12:06:58) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerApp.java
@@ -1,11 +1,14 @@
 package ru.labs.anonymizer.core;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class AnonymizerApp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("anonymizer-system");
 
-
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }

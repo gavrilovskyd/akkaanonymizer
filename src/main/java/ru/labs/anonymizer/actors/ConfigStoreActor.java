c78d50d3d4e9d57d1f57d71d0e6d69924b965cbe
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 12:20:36 +0300

    Scripted auto-commit on change (2019-11-18 12:20:36) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/actors/ConfigStoreActor.java
+++ src/main/java/ru/labs/anonymizer/actors/ConfigStoreActor.java
@@ -1,4 +1,13 @@
 package ru.labs.anonymizer.actors;
 
-public class ConfigStoreActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class ConfigStoreActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+            .build();
+    }
 }

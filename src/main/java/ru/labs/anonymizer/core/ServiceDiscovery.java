93cfea7dd4201c40f70643fddb852a6b0bada7fc
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-18 13:57:25 +0300

    Scripted auto-commit on change (2019-11-18 13:57:25) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
+++ src/main/java/ru/labs/anonymizer/core/ServiceDiscovery.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class ServiceDiscovery {
     private static final int SESSION_TIMEOUT = 3000; // ms
-    private static final String REGISTRY_NODE_PATH = "/"
+    private static final String REGISTRY_NODE_PATH = "/servers/s";
 
     private ZooKeeper zoo;
 

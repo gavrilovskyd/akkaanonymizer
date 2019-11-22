f5abbe3741fc492daaedbda712691a1a6a9f15dd
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-22 23:32:20 +0300

    Scripted auto-commit on change (2019-11-22 23:32:20) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
@@ -81,8 +81,10 @@ public class AnonymizerRoutes extends AllDirectives {
                         Pair.create(COUNT_NAME, Integer.toString(count - 1))
                     ));
                 return fetch(redirectUri.toString())
-                    .whenCompleteAsync((result, exception -> {
-                        
+                    .whenCompleteAsync((result, exception) -> {
+                        if (exception == null) {
+                            return result;
+                        }
                     }));
             });
     }

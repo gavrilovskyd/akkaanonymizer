fade58e16ebea9d05e19d474dd91b6ebe6caecb9
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-22 19:16:54 +0300

    Scripted auto-commit on change (2019-11-22 19:16:54) by gitwatch.sh

--- src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
+++ src/main/java/ru/labs/anonymizer/core/AnonymizerRoutes.java
@@ -36,7 +36,7 @@ public class AnonymizerRoutes extends AllDirectives {
         final ExceptionHandler wrongParameterHandler = ExceptionHandler.newBuilder()
             .match(NumberFormatException.class, e ->
                 complete(StatusCodes.BAD_REQUEST, "count must be integer"))
-            .match(StreamTcpException.class, e ->
+            .match(.class, e ->
                 complete(StatusCodes.BAD_REQUEST, "can not connect to provided url"))
             .build();
 
